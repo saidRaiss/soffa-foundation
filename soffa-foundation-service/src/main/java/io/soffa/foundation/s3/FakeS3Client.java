492e8e51484e963c0bee448cd7b76081f07f7049
Titus GAHISSY <titus@gahissy.com>
2021-10-09 13:00:19 +0100

    S3 client added

---
+++ soffa-foundation-service/src/main/java/io/soffa/foundation/s3/FakeS3Client.java
@@ -0,0 +1,56 @@
+package io.soffa.foundation.s3;
+
+import io.soffa.foundation.logging.Logger;
+
+import java.io.File;
+import java.io.InputStream;
+
+public class FakeS3Client implements ObjectStorageClient {
+
+    private static final Logger LOG = Logger.get(FakeS3Client.class);
+
+    @Override
+    public void upload(InputStream source, String bucket, String objectName, String contentType) {
+        LOG.info("FakeS3: upload to %s/%s", bucket, objectName);
+    }
+
+    @Override
+    public void upload(File source, String bucket, String objectName) {
+        LOG.info("FakeS3: upload file to to %s/%s", bucket, objectName);
+    }
+
+    @Override
+    public void upload(File source, String objectName) {
+        LOG.info("FakeS3: upload file to to %s", objectName);
+    }
+
+    @Override
+    public void upload(InputStream source, String objectName, String contentType) {
+        LOG.info("FakeS3: upload stream to to %s", objectName);
+    }
+
+    @Override
+    public String getDownloadUrl(String bucket, String objectName, long expiresInMinutes) {
+        return "mocked://" + bucket + "/" + objectName;
+    }
+
+    @Override
+    public String getDownloadUrl(String objectName, long expiresInMinutes) {
+        return "mocked://default/" + objectName;
+    }
+
+    @Override
+    public String getUploadUrl(String bucket, String objectName, long expiresInMinutes) {
+        return "mocked://" + bucket + "/" + objectName;
+    }
+
+    @Override
+    public String getUploadUrl(String objectName, long expiresInMinutes) {
+        return "mocked://default/" + objectName;
+    }
+
+	@Override
+	public String downloadBase64(String bucket, String objectName) {
+		return "";
+	}
+}

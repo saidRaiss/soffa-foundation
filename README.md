8032dc701cf9d2b7ae22fd017c8646926744bd78
SAID RAISS <said.raiss@socgen.com>
2022-01-18 15:53:03 +0100

    update doc + javadoc

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### Foundation
 
-![Discuss](https://img.shields.io/badge/release-1.2.8-green.svg?style=flat)
+![Discuss](https://img.shields.io/badge/release-1.2.9-green.svg?style=flat)
 
--- soffa-foundation-api/src/main/java/io/soffa/foundation/core/ApiHeaders.java
+++ soffa-foundation-api/src/main/java/io/soffa/foundation/core/ApiHeaders.java
@@ -6,11 +6,22 @@ public interface ApiHeaders {
     String APPLICATION = "X-Application";
     String TRACE_ID = "X-TraceId";
     String SPAN_ID = "X-SpanId";
-
+    /**
+     * @deprecated <p>
+     * use {@link SecuritySchemes#BEARER_AUTH} instead.
+     */
     @Deprecated
     String JWT = "BEARER_AUTH";
+    /**
+     * @deprecated
+     * <p>use {@link SecuritySchemes#BASIC_AUTH} instead.
+     */
     @Deprecated
     String BASIC_AUTH = "BASIC_AUTH";
+    /**
+     * @deprecated
+     * <p>use {@link SecuritySchemes#OAUTH2} instead.
+     */
     @Deprecated
     String OAUTH2 = "OAUTH2";
 }
